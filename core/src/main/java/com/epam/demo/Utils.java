37b6d8222474ab9090622446cb8ff52c27a17e3b
Stanly0407 <Shelestova_S_V@mail.ru>
2021-08-12 22:42:16 +0300

    util classes

---
+++ core/src/main/java/com/epam/demo/Utils.java
@@ -0,0 +1,11 @@
+package com.epam.demo;
+
+import java.util.Arrays;
+
+
+public class Utils {
+
+    public static boolean isAllPositiveNumbers(String... str) {
+        return Arrays.stream(str).allMatch(StringUtils::isPositiveNumber);
+    }
+}
