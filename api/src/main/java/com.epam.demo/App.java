b844afd540aadffc8b6ed6bc8def607f3794aa87
Stanly0407 <Shelestova_S_V@mail.ru>
2021-08-16 23:05:18 +0300

    methods correct.

--- api/src/main/java/com.epam.demo/App.java
+++ api/src/main/java/com.epam.demo/App.java
@@ -2,7 +2,10 @@ package com.epam.demo;
 
 
 public class App {
+
     public static void main(String[] args) {
-        System.out.println(Utils.isAllPositiveNumbers("12", "79"));
+        System.out.println("Is all positive number? \n" + Utils.isAllPositiveNumbers(args));
     }
+
 }
+
--- core/src/main/java/com/epam/demo/Utils.java
+++ core/src/main/java/com/epam/demo/Utils.java
@@ -1,11 +1,29 @@
 package com.epam.demo;
 
-import java.util.Arrays;
 
+import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class Utils {
 
+    private static final String NUMBER_PATTERN = "^[-]?(([1-9]\\d*)|0)(\\.\\d+)?";
+
     public static boolean isAllPositiveNumbers(String... str) {
+        if (str.length == 0) {
+            System.out.println("Running without arguments, default values: \"12\", \"79\" will be used.");
+            str = new String[]{"12", "79"};
+        } else if (!isStringNumber(str)) {
+            System.out.println("Input error, not a number entered.");
+        } else {
+            System.out.println("Run with arguments: " + Arrays.toString(str));
+        }
         return Arrays.stream(str).allMatch(StringUtils::isPositiveNumber);
     }
+
+    private static boolean isStringNumber(String... str) {
+        Pattern numberPattern = Pattern.compile(NUMBER_PATTERN);
+        return Arrays.stream(str).allMatch(s -> numberPattern.matcher(s).matches());
+    }
+
 }
+
